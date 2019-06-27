c97d6affff3e7683c10eca312672056f63aec3f4
maheshsabnis <sabnis_m@hotmail.com>
2019-06-27 13:34:39 +0530

    Add files via upload

---
+++ DepartmentService.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CoreApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoreApp.Services
+{
+    public class DepartmentService : IService<Department, int>
+    {
+        private readonly MyAppDbCobtext _ctx;
+
+        /// <summary>
+        /// Inject the DbContext in the ctor
+        /// </summary>
+        public DepartmentService(MyAppDbCobtext ctx)
+        {
+            _ctx = ctx;
+        }
+        public async Task<Department> CreateAsync(Department entity)
+        {
+            var res = await _ctx.Departments.AddAsync(entity);
+            await _ctx.SaveChangesAsync();
+            return res.Entity;
+        }
+
+        public async Task<bool> DeleteAsync(int id)
+        {
+            var res = await _ctx.Departments.FindAsync(id);
+            if (res != null)
+            {
+                _ctx.Departments.Remove(res);
+                await _ctx.SaveChangesAsync();
+                return true;
+            }
+            return false;
+        }
+
+        public async Task<IEnumerable<Department>> GetAsync()
+        {
+            var res = await _ctx.Departments.ToListAsync();
+            return res;
+        }
+
+        public async Task<Department> GetAsync(int id)
+        {
+            var res = await _ctx.Departments.FindAsync(id);
+            return res;
+        }
+
+        public async Task<Department> UpdateAsync(int id, Department entity)
+        {
+            var res = await _ctx.Departments.FindAsync(id);
+            if (res != null)
+            {
+                res.DeptNo = entity.DeptNo;
+                res.DeptName = entity.DeptName;
+                res.Location = entity.Location;
+                res.Capacity = entity.Capacity;
+                await _ctx.SaveChangesAsync();
+            }
+            return res;
+        }
+    }
+}
---
+++ EmployeeService.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CoreApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoreApp.Services
+{
+    public class EmployeeService : IService<Employee, int>
+    {
+        private readonly MyAppDbCobtext _ctx;
+
+        /// <summary>
+        /// Inject the DbContext in the ctor
+        /// </summary>
+        public EmployeeService(MyAppDbCobtext ctx)
+        {
+            _ctx = ctx;
+        }
+        public async Task<Employee> CreateAsync(Employee entity)
+        {
+            var res = await _ctx.Emplopyees.AddAsync(entity);
+            await _ctx.SaveChangesAsync();
+            return res.Entity;
+        }
+
+        public async Task<bool> DeleteAsync(int id)
+        {
+            var res = await _ctx.Emplopyees.FindAsync(id);
+            if (res != null)
+            {
+                _ctx.Emplopyees.Remove(res);
+                await _ctx.SaveChangesAsync();
+                return true;
+            }
+            return false;
+        }
+
+        public async Task<IEnumerable<Employee>> GetAsync()
+        {
+            var res = await _ctx.Emplopyees.ToListAsync();
+            return res;
+        }
+
+        public async Task<Employee> GetAsync(int id)
+        {
+            var res = await _ctx.Emplopyees.FindAsync(id);
+            return res;
+        }
+
+        public async Task<Employee> UpdateAsync(int id, Employee entity)
+        {
+            var res = await _ctx.Emplopyees.FindAsync(id);
+            if (res != null)
+            {
+                res.EmpNo = entity.EmpNo;
+                res.EmpName = entity.EmpName;
+                res.Designation = entity.Designation;
+                res.Salary = entity.Salary;
+                res.DeptRowId = entity.DeptRowId;
+                await _ctx.SaveChangesAsync();
+            }
+            return res;
+        }
+    }
+}
